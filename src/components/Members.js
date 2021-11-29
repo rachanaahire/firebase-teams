a47ed60946f01c584033e32e393fedcefbaa8492
RACHANA AHIRE <rachanaahire98@gmail.com>
2021-11-29 13:54:48 +0530

    Members component done

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core';
-import { getMembersOfProject } from '../firebase';
+import { makeStyles, Accordion, AccordionDetails, AccordionSummary, Typography } from '@material-ui/core';
+import { ExpandMore } from '@material-ui/icons';
+import { getEditorNamesByProjectId, getManagerNamesByProjectId, getMembersOfProject, getUserNameByUid } from '../firebase';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,13 +12,24 @@ const useStyles = makeStyles((theme) => ({
 const Members = (props) => {
     const classes = useStyles();
     const [loading, setLoading] = useState(false);
-    const [members, setMembers] = useState({});
+    const [owner, setOwner] = useState('');
+    const [managers, setManagers] = useState(null);
+    const [editors, setEditors] = useState(null);
+    const [expanded, setExpanded] = useState(false);
     let team = props.team;
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
     const getMembers = async () => {
         setLoading(true);
-        let members = await getMembersOfProject(team.docId);
-        setMembers(members);
+        let ownerName = await getUserNameByUid(team.owner);
+        setOwner(ownerName);
+        let managerNames = await getManagerNamesByProjectId(team.docId);
+        setManagers(managerNames);
+        let editorsNames = await getEditorNamesByProjectId(team.docId);
+        setEditors(editorsNames);
         setLoading(false);
     }
 
@@ -29,10 +41,50 @@ const Members = (props) => {
         return <h1>LOADING</h1>
     }
 
+
     return (
         <> <h1>Members</h1>
+            <div>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMore />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography> <b> Owner</b></Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>{owner}</Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMore />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography><b>Managers</b></Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography> {managers && managers.map((name) => <>{name} <br /></>)} </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMore />}
+                        aria-controls="panel2a-content"
+                        id="panel2a-header"
+                    >
+                        <Typography><b>Editors</b></Typography>
+                    </AccordionSummary>
+                    <AccordionDetails> {editors && editors.map((name) => <>{name} <br /></>)}
+                    </AccordionDetails>
+                </Accordion>
+            </div>
         </>
     );
+
+
 }
 
 export default Members;
